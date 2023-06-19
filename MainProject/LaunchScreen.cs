3a7979bf0e8cf14fc0d78014394bffb037db3bed
Denny Cheng <cdfq152313@gmail.com>
2023-06-19 16:52:38 +0800

    版本紀錄

---
+++ MainProject/LaunchScreen.cs
@@ -0,0 +1,16 @@
+using System;
+using Godot;
+
+public partial class LaunchScreen : Node2D
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Console.WriteLine("Hello world");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+}
