8d11d1c2e74defeed4be374ec52ce96fbe86e9a3
Denny Cheng <cdfq152313@gmail.com>
2023-06-22 22:04:50 +0800

    框架

--- MainProject/LaunchScreen.cs
+++ MainProject/LaunchScreen.cs
@@ -1,21 +1,24 @@
+using GdExtension;
 using Godot;
 
 public partial class LaunchScreen : Node2D
 {
-    [Signal]
-    public delegate void MySignalEventHandler();
+	[Signal]
+	public delegate void MySignalEventHandler();
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        GD.Print("Hello Ready!");
-        GeneratedClass.GeneratedMethod();
-        MySignal += () => GD.Print("Hello!");
-        EmitSignal(SignalName.MySignal);
-    }
+	[OnReadyNode]
+	private Node _hello;
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Hello Ready!");
+		MySignal += () => GD.Print("Hello!");
+		EmitSignal(SignalName.MySignal);
+	}
 
-    partial void HelloFrom(string name);
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) { }
+
+	partial void HelloFrom(string name);
 }
