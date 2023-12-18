7d21a11b8bb45686567af38803f997c533ed22f3
Denny Cheng <cdfq152313@gmail.com>
2023-12-18 17:44:32 +0800

    Prepare for nuget

--- nuget.sh
+++ nuget.sh
@@ -12,3 +12,4 @@ echo "版本號已更新為：${new_version}"
 
 # 上傳 nuget
 dotnet.exe pack ./GodotSharpKit/GodotSharpKit.csproj
+dotnet.exe nuget push ./GodotSharpKit/bin/Release/GodotSharpKit.${new_version}.nupkg
