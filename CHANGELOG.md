c0af88ddd0d01c13a7be1f1f7b0e3e417e1aab23
Denny Cheng <cdfq152313@gmail.com>
2023-12-18 17:24:20 +0800

    set for nuget

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+﻿# 1.2.2
+- Fix signal generator cannot use generic type. 
+
+# 2.0.0
+- Combine two nuget package to one.
---
+++ nuget.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# 檢查參數是否正確
+if [ $# -ne 1 ]; then
+    echo "請提供正確的版本號作為參數，例如：./nuget.sh 2.0.0"
+    exit 1
+fi
+
+# 替換版本號
+new_version="$1"
+sed -i "s|<Version>.*</Version>|<Version>${new_version}</Version>|g" GodotSharpKit/GodotSharpKit.csproj
+echo "版本號已更新為：${new_version}"
+
+# 上傳 nuget
+dotnet.exe pack ./GodotSharpKit/GodotSharpKit.csproj
