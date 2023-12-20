4a392eafd5b6d49416a45f993844d6581466b228
Denny Cheng <cdfq152313@gmail.com>
2023-12-20 22:11:10 +0800

    Fix AutoResDemo

--- MainProject/AutoResDemo/AutoResDemoScreen.cs
+++ MainProject/AutoResDemo/AutoResDemoScreen.cs
@@ -1,8 +1,28 @@
 using Godot;
+using Godot4Demo.AutoResDemo.Deep.Deep2;
+using GodotSharpKit.Misc;
 
 namespace Godot4Demo.AutoResDemo;
 
 public partial class AutoResDemoScreen : Node2D
 {
-    public override void _Ready() { }
+    public override void _Ready()
+    {
+        // Resource and Path
+        string imagePath = AutoRes.icon.Path;
+        GD.Print(imagePath);
+        Resource imageRes = AutoRes.icon.Load();
+
+        // Typed Resource
+        string fontPath = Fonts.MySystemFont.Path;
+        GD.Print(fontPath);
+        Font font = Fonts.MySystemFont.Load();
+
+        // Special case: packed scene
+        string deepNodePath = A.DeepNode.Path;
+        GD.Print(deepNodePath);
+        PackedScene deepNodePackedScene = A.DeepNode.Load();
+        SceneFactory<DeepNode> deepNodeFactory = A.DeepNode.Factory;
+        DeepNode deepNodeInstance = deepNodeFactory.Instantiate();
+    }
 }
